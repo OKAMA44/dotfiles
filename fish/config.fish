ee6bcb816f88e8fcab615ec1ad0a9250006818cd
OKAMA44 <okamanakamura4@gmail.com>
2026-06-04 23:48:13 -0300

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+alias neofetch 'fastfetch -l arch2 -c /examples/6'
+alias cbonsai 'cbonsai --screensaver --wait=1'
+alias cmatrix 'rmatrix'
+
+neofetch
+end
