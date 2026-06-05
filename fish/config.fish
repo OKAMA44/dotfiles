cdf6be416e73bba5af112370d541bc4fdba0d088
OKAMA44 <okamanakamura4@gmail.com>
2026-06-04 23:43:36 -0300

    Delete fish directory

--- fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-# Commands to run in interactive sessions can go here
-alias neofetch 'fastfetch -l arch_old -c /examples/6'
-alias cbonsai 'cbonsai --screensaver --wait=1'
-alias cmatrix 'rmatrix'
-
-neofetch
-end
