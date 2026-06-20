d0cbd49d9f4ed05b140b3638726ec161bb6fb5b6
OKAMA44 <okamanakamura4@gmail.com>
2026-06-19 23:36:04 -0300

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,11 @@ alias cbonsai 'cbonsai --screensaver --wait=1'
 alias cmatrix 'rmatrix'
 alias ls 'lsd -a1'
 
+function fish_prompt
+    printf '󰣇 '
+    printf (prompt_pwd) 
+    printf ' > '
+end
+
 pokemon-colorscripts -r
 end
